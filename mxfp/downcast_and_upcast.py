12b6437a4e4f8b49b8b1c92b6a45005f281d3a47
Cheng Zhang <chengzhang98@outlook.com>
2025-06-20 19:17:18 +0100

    mxfp cast example; mxfp w-only example

---
+++ mxfp/downcast_and_upcast.py
@@ -0,0 +1,87 @@
+"""
+This script demonstrates how to downcast/quantize a BF16 tensor to MXFP, and upcast it back to BF16.
+"""
+
+import torch
+from mase_triton.mxfp.functional import compose_mxfp_tensor, extract_mxfp_components
+from mase_triton.mxfp.meta import (
+    OCP_MXFP4_E2M1,
+    OCP_MXFP6_E2M3,
+    OCP_MXFP6_E3M2,
+    OCP_MXFP8_E4M3,
+    OCP_MXFP8_E5M2,
+    MXFPMeta,
+)
+from mase_triton.utils.bit_repr import get_binary_repr, get_binary_repr_bf16
+from mase_triton.utils.train_utils import set_seed
+
+set_seed(42)  # for reproducibility
+
+
+def quantize():
+    torch.set_printoptions(linewidth=200)
+    # define a mxfp format
+    mxfp_format = MXFPMeta(
+        block_size=2,  # a small block size for demonstration
+        scale_exp_bits=8,  # the bit width of the shared exponent
+        element_exp_bits=4,  # the bit width of the element (MiniFloat) exponent
+        element_frac_bits=3,  # the bit width of the element (MiniFloat) fraction
+    )
+
+    w = torch.randn((3, 2), dtype=torch.bfloat16, device="cuda") * 100.0
+    print("Original tensor:")
+    print(get_binary_repr_bf16(w))
+    # quantize the tensor to MXFP format, extracting shared scales and elements
+    # tensor_meta contains the metadata of the MXFP tensor, like original shape, the dimension to share scales, device, etc.
+    scales, elements, tensor_meta = extract_mxfp_components(
+        w, block_dim=1, mxfp_meta=mxfp_format
+    )
+    print("Tensor Meta data")
+    print(tensor_meta)
+    print("Shared scales:")
+    print(get_binary_repr(scales))
+    print("Elements (Minifloat)")
+    print(get_binary_repr(elements))
+    """outputs:
+    Original tensor:
+    [['1 10000110 0001011' '0 10000010 1010011']
+    ['0 10000100 0101111' '0 10000110 1000101']
+    ['1 10000111 0001001' '1 10000100 1011111']]
+    Tensor Meta data
+    MXFPTensorMeta(device='cuda:0', shape=(3, 2), block_dim=1, meta=MXFPMeta(block_size=2, scale_exp_bits=8, element_exp_bits=4, element_frac_bits=3))
+    Shared scales:
+    [['1000 0110']
+    ['1000 0110']
+    ['1000 0111']]
+    Elements (Minifloat)
+    [['1011 1000' '0001 1101']
+    ['0010 1010' '0011 1100']
+    ['1011 1000' '1010 0101']]
+    """
+
+
+def quantize_and_dequantize():
+    torch.set_printoptions(linewidth=200)
+    mxfp_format = OCP_MXFP8_E5M2
+
+    w = torch.randn((3, 64), dtype=torch.bfloat16, device="cuda") * 100.0
+    scales, elements, tensor_meta = extract_mxfp_components(
+        w, block_dim=1, mxfp_meta=mxfp_format
+    )
+    # dequantize the MXFP tensor back to BF16
+    w_mxfp8 = compose_mxfp_tensor(scales, elements, tensor_meta)
+    avg_err = (w - w_mxfp8).abs().mean().item()
+    err_ratio = avg_err / w.abs().mean().item()
+    print(f"Tensor Meta data: {tensor_meta}")
+    print("Mean abs error: ", avg_err)
+    print("Error ratio: ", err_ratio)
+    """outputs:
+    Tensor Meta data: MXFPMeta(block_size=32, scale_exp_bits=8, element_exp_bits=5, element_frac_bits=2)
+    Mean abs error:  6.25
+    Error ratio:  0.08223684210526316
+    """
+
+
+if __name__ == "__main__":
+    quantize()
+    quantize_and_dequantize()
---
+++ mxfp/lm_eval_ppl.py
@@ -0,0 +1,108 @@
+"""
+This script shows how to replace the linear layers of a Hugging Face model with MXFPLinearPTQ layers,
+and evaluate the model's perplexity on the Wikitext dataset using lm-eval.
+"""
+
+from typing import Literal, Union
+
+import torch
+from lm_eval.evaluator import simple_evaluate
+from lm_eval.models.huggingface import HFLM
+from lm_eval.utils import make_table
+from mase_triton.mxfp.layers import MXFPLinearPTQ
+from mase_triton.mxfp.meta import (
+    OCP_MXFP4_E2M1,
+    OCP_MXFP6_E2M3,
+    OCP_MXFP6_E3M2,
+    OCP_MXFP8_E4M3,
+    OCP_MXFP8_E5M2,
+    MXFPMeta,
+)
+from mase_triton.utils.torch_module import set_layer_by_name
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+
+def replace_fc(model: torch.nn.Module, fc_kwargs: dict, skip_layers: list[str] = None):
+    """
+    Replace all Linear layers in the model with MXFPLinearPTQ layers.
+    """
+    replaced = 0
+    for name, layer in model.named_modules():
+        if not isinstance(layer, torch.nn.Linear):
+            continue
+        if skip_layers is not None and name in skip_layers:
+            print(f"Skipping layer: {name}")
+            continue
+        new_fc = MXFPLinearPTQ.from_linear(layer=layer, **fc_kwargs)
+        set_layer_by_name(model, name, new_fc)
+        replaced += 1
+
+    print(f"Replaced {replaced} layers")
+    return model
+
+
+PRESET_MAP = {
+    "MXFP8_E4M3": OCP_MXFP8_E4M3,
+    "MXFP8_E5M2": OCP_MXFP8_E5M2,
+    "MXFP6_E2M3": OCP_MXFP6_E2M3,
+    "MXFP6_E3M2": OCP_MXFP6_E3M2,
+    "MXFP4_E2M1": OCP_MXFP4_E2M1,
+}
+
+
+def eval_wikitext_ppl(
+    model_name: str = "unsloth/Llama-3.2-1B",
+    w_meta: Union[
+        MXFPMeta,
+        Literal["MXFP8_E4M3", "MXFP8_E5M2", "MXFP6_E2M3", "MXFP6_E3M2", "MXFP4_E2M1"],
+        None,
+    ] = "MXFP8_E4M3",
+):
+    """
+    Evaluate the perplexity of a model on the Wikitext dataset with all weights of the linear layers (except lm_head) quantized with MXFP.
+
+    Args:
+        model_name (str): The name of the model to evaluate.
+        w_meta (Union[MXFPMeta, str, None]): The metadata for the weights of the linear layers. If None, no quantization is applied.
+            Can also be one of the following strings: "MXFP8_E4M3", "MXFP8_E5M2", "MXFP6_E2M3", "MXFP6_E3M2", "MXFP4_E2M1".
+    """
+    tasks = ["wikitext"]
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    # create the quantization config (MXFPMeta) for MXFPLinearPTQ
+    if w_meta is not None and not isinstance(w_meta, MXFPMeta):
+        w_meta = PRESET_MAP[w_meta]
+
+    q_config = {
+        "x_mxfp_meta": None,  # No quantization for input activation
+        "w_mxfp_meta": w_meta,  # Quantization metadata for weights
+        "b_mxfp_meta": None,  # No quantization for bias
+        "layer_type": "XWqB",  # Only the weight is quantized (Wq)
+        "backend": "separate",  # Use 'separate' backend for quantization. For now the fused backend is not implemented.
+    }
+
+    # create the tokenizer and model
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16)
+    # replace the linear layers with MXFPLinearPTQ layers, except for the lm_head
+    if w_meta is not None:
+        model = replace_fc(model, fc_kwargs=q_config, skip_layers=["lm_head"])
+    else:
+        print("Skipping quantization, using original model weights.")
+    model = model.to(device)
+    # wrap the model with lm-eval's HFLM
+    model_lm_eval = HFLM(pretrained=model, tokenizer=tokenizer, max_length=2048)
+    # pass the wrapped model to the lm-eval's evaluator
+    results = simple_evaluate(
+        model=model_lm_eval, tasks=tasks, batch_size="auto", log_samples=False
+    )
+    # print the results
+    table = make_table(results)
+    print(w_meta)
+    print(table)
+
+
+if __name__ == "__main__":
+    from jsonargparse import CLI
+
+    CLI(eval_wikitext_ppl)
